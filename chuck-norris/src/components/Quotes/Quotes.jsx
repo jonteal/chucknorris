2dd83db0fb9e6d1900a40c84b5600586e1aadb97
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-03-11 20:58:51 -0600

    Receiving data object with fetch function. Now working on displaying onscreen

--- chuck-norris/src/components/Quotes/Quotes.jsx
+++ chuck-norris/src/components/Quotes/Quotes.jsx
@@ -1,11 +1,36 @@
 import React from "react";
 import axios from 'axios';
+import { useState } from "react";
 
+const fetchFact = () => {
 
+    return axios
+    .get("https://api.chucknorris.io/jokes/random")
+    .then(({ data }) => {
+        // console.log(data);
+
+        return data.value;
+    })
+    .then((err) => {
+        console.error(err)
+    })
+
+}
 
 const Quotes = () => {
+
+    const [quote, setQuote] = useState('');
+    const [nextQuote, setNextQuote] = useState('');
+
     return(
-        <h1>Hello</h1>
+        <div>
+            <button
+                onClick={() => {
+                    fetchFact(setQuote);
+                }}
+            >Click for Chuck Norris Facts</button>
+            <p>{quote}</p>
+        </div>
     )
 }
 
